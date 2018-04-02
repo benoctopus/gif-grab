c68b8f3442db15a65be66c5013558a4b5be6fe1b
Benjamin <rose.benjamin925@gmail.com>
2018-04-01 22:50:19 -0700

    sidebar load in js added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,39 @@
+// window.apiKey = "UqSUUkDz0IyApK3toBVqHbtloo27LlFNV";
+
+class Meme {
+  constructor(meme) {
+    this.meme = meme;
+
+
+  }
+
+  createButton() {
+
+  }
+}
+
 $(document).ready(function () {
-  window.apiKey = "UqSUUkDz0IyApK3toBVqHbtloo27LlFNV";
+  loadMemes();
+  sidebarListener();
 });
 
-$("#menu-toggle").click(function (e) {
-  e.preventDefault();
-  $("#wrapper").toggleClass("toggled");
-});
+function sidebarListener() {
+  let sideBar = $("#sidebar-wrapper") ;
+  let introText = $("#instruction-row");
+  sideBar.hover(function () {
+    if (introText.css("display") !== "none") {
+      introText.fadeOut(500)
+    }
+    $("#wrapper").toggleClass("toggled");
+  });
+}
+
+function loadMemes() {
+  if (typeof localStorage.memes !== "undefined") {
+    window.memes = JSON.parse(localStorage.memes)
+  }
+  else {
+    window.memes = [];
+    $("#instruction-row").fadeIn(1000);
+  }
+}
