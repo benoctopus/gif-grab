58ee0467fa82de4afde61d7b36c518b347e89560
Benjamin <rose.benjamin925@gmail.com>
2018-04-02 14:56:31 -0700

    removed unnecisary code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,18 +2,18 @@
 
 class Meme {
   constructor(meme, store) {
-    this.name = meme;
-    this.createButton(meme);
+    this.name = format(meme);
+    this.createButton(this.name);
     appendLocal(this.name, this)
   }
 
   createButton(meme) {
     let sideElem = $(
-      `<li data-id="${this.name}" id="list-${meme}" style="display: none;">`
+      `<li data-id="${meme}" id="list-${meme}" style="display: none;">`
       + `<a data-function="kill" id="kill-${meme}" `
       + `class="link-X meme-category" href="#">X</a>`
       + `<a data-function="display" id="display-${meme}" `
-      + `class="link meme-category" href="#">${meme}</a>`
+      + `class="link meme-category" href="#">${deFormat(meme)}</a>`
       + `</li>`
     );
     window.sidebar.append(sideElem);
@@ -26,6 +26,7 @@ class Meme {
     removeLocal(this.name);
     buttonListener();
   }
+
 }
 
 $(document).ready(function () {
@@ -36,33 +37,44 @@ $(document).ready(function () {
 });
 
 function sidebarListener() {
-  let sideBar = $("#sidebar-wrapper");
+  let sideWrap = $("#sidebar-wrapper");
   let introText = $("#instruction-row");
-  sideBar.on("mouseenter mouseleave", function (event) {
+  console.log(sideWrap.width());
+  sideWrap.on("mouseenter mouseleave", function (event) {
+    console.log(event, this, $(this));
     // event.preventDefault();
     if (introText.css("display") !== "none") {
       introText.fadeOut(500)
     }
-    $("#wrapper").toggleClass("toggled");
+    if ((sideWrap.width() < 20)
+      && event.type === "mouseenter") {
+      $("#wrapper").toggleClass("toggled");
+    }
+    else if ((sideWrap.width() > 200)
+      && event.type === "mouseleave") {
+      $("#wrapper").toggleClass("toggled");
+    }
   });
 }
 
 function buttonListener() {
   $("a").on("click", function (event) {
     let a = $("a");
-    a.off("click");
-
     let clicked = $(this);
     let id = clicked.attr("id");
     console.log(clicked);
     if (id === "add-button") {
-      let input = $("#search-bar").val();
+      let input = $("#search-bar").val().trim();
+      a.off("click");
       if (input.length > 0) {
-        let obj = new Meme(input.trim());
+        let obj = new Meme(input);
+      }
+      else {
+        buttonListener();
       }
     }
-
     else if (clicked.attr("data-function") === "kill") {
+      a.off("click");
       console.log(clicked.parent().attr("data-id"));
       a.off("mouseleave mouseenter");
       clicked.parent().fadeOut(500, function () {
@@ -88,6 +100,15 @@ function loadMemes() {
     $("#instruction-row").fadeIn(1000);
   }
 }
+
+function format(string) {
+  return string.replace(/ /g, "-")
+}
+
+function deFormat(string) {
+  return string.replace(/-/g, " ")
+}
+
 
 function appendLocal(string, object) {
   window.activeMemes[string] = object;
