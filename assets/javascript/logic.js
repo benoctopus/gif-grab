a4eac2eb59907e98a60087c49a12d16a41e79b5c
Benjamin <rose.benjamin925@gmail.com>
2018-04-01 18:42:07 -0700

    added sidebar html

---
+++ assets/javascript/logic.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  window.apiKey = "UqSUUkDz0IyApK3toBVqHbtloo27LlFNV";
+});
+
+$("#menu-toggle").click(function (e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
